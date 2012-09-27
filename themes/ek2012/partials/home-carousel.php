073d9448a5939d2accc68325897b63c81f7dc3f5
jessica <jessica@exobi.com>
2012-09-27 00:40:04 -0400

    Carousel vertical position updated

--- themes/ek2012/partials/home-carousel.php
+++ themes/ek2012/partials/home-carousel.php
@@ -1,15 +1,15 @@
 <div id="feature" class="row">
 	<div class="span8">
 		<div id="featured-carousel" class="carousel slide">
-		<!-- Carousel items -->
-		<div class="carousel-inner">
-			<div class="active item"><img src="http://fpoimg.com/624x350" alt=""></div>
-			<div class="item"><img src="http://fpoimg.com/624x350" alt=""></div>
-			<div class="item"><img src="http://fpoimg.com/624x350" alt=""></div>
-		</div>
-		<!-- Carousel nav -->
-		<a class="carousel-control left" href="#featured-carousel" data-slide="prev">&lsaquo;</a>
-		<a class="carousel-control right" href="#featured-carousel" data-slide="next">&rsaquo;</a>
+			<!-- Carousel items -->
+			<div class="carousel-inner">
+				<div class="active item"><img src="http://fpoimg.com/624x350" alt=""></div>
+				<div class="item"><img src="http://fpoimg.com/624x350" alt=""></div>
+				<div class="item"><img src="http://fpoimg.com/624x350" alt=""></div>
+			</div>
+			<!-- Carousel nav -->
+			<a class="carousel-control left" href="#featured-carousel" data-slide="prev">&lsaquo;</a>
+			<a class="carousel-control right" href="#featured-carousel" data-slide="next">&rsaquo;</a>
 		</div>
 	</div>
 	<div class="span4">
