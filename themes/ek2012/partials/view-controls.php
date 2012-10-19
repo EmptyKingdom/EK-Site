4057fa25e443561bf03026b169ae7acea1dd4f15
jessica <jessica@exobi.com>
2012-10-18 20:01:09 -0400

    Category filters populated from db

--- themes/ek2012/partials/view-controls.php
+++ themes/ek2012/partials/view-controls.php
@@ -7,56 +7,40 @@
 			<a id="close-cat-filters" href="javascript:void(0)"></a>
 			<h3>Select specific categories you want to view.</h3>
 			<div class="row">
-				<div class="span2 illustration">
+				<div class="span2 illustration-art">
 					<h5>Illustration &amp; Art</h5>
-					<ul class="cat-filter illustration unstyled">
-						<li><a href="javascript:void(0)"><span class="check"></span>Collage</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Digital</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Drawings</a></li>
-						<li class="selected"><a href="javascript:void(0)"><span class="check"></span>Fine Art</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Graphic</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Illustration</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Installation</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Mixed Media</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Obscure</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Painting</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Sculpture</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Street</a></li>
+					<ul class="cat-filter illustration-art unstyled">
+						<?php $cats = get_categories(array('child_of' => 4)); ?>
+						<?php foreach ($cats as $cat) : ?>
+						<li data-cat_id="<?php echo $cat->ID ?>"><a href="javascript:void(0)"><span class="check"></span><?php echo $cat->name ?></a></li>
+						<?php endforeach; ?>
 					</ul> <!-- /.cat-filter.illustration -->
 				</div> <!-- .span2 -->
 				<div class="span2 photography">
 					<h5>Photography</h5>
 					<ul class="cat-filter photography unstyled">
-						<li><a href="javascript:void(0)"><span class="check"></span>Abstract</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Ambient</a></li>
-						<li class="selected"><a href="javascript:void(0)"><span class="check"></span>Black &amp; White</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Commercial / Editorial</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Fashion</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Journalism</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Landscape</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Nude</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Portrait</a></li>
+						<?php $cats = get_categories(array('child_of' => 6)); ?>
+						<?php foreach ($cats as $cat) : ?>
+						<li data-cat_id="<?php echo $cat->ID ?>"><a href="javascript:void(0)"><span class="check"></span><?php echo $cat->name ?></a></li>
+						<?php endforeach; ?>
 					</ul> <!-- /.cat-filter.photography -->
 				</div> <!-- .span2 -->
 				<div class="span2 film">
 					<h5>Film</h5>
 					<ul class="cat-filter film unstyled">
-						<li><a href="javascript:void(0)"><span class="check"></span>Animation</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Commercial</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Feature Film</a></li>
-						<li class="selected"><a href="javascript:void(0)"><span class="check"></span>Music Video</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Short Film</a></li>
+						<?php $cats = get_categories(array('child_of' => 3)); ?>
+						<?php foreach ($cats as $cat) : ?>
+						<li data-cat_id="<?php echo $cat->ID ?>"><a href="javascript:void(0)"><span class="check"></span><?php echo $cat->name ?></a></li>
+						<?php endforeach; ?>
 					</ul> <!-- /.cat-filter.film -->
 				</div> <!-- .span2 -->
 				<div class="span2 new-media">
 					<h5>New Media</h5>
 					<ul class="cat-filter new-media unstyled">
-						<li><a href="javascript:void(0)"><span class="check"></span>Advertising</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Animation</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Graphic Design</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Motion Graphics</a></li>
-						<li><a href="javascript:void(0)"><span class="check"></span>Typography</a></li>
-						<li class="selected"><a href="javascript:void(0)"><span class="check"></span>Visual Effects</a></li>
+						<?php $cats = get_categories(array('child_of' => 5)); ?>
+						<?php foreach ($cats as $cat) : ?>
+						<li data-cat_id="<?php echo $cat->ID ?>"><a href="javascript:void(0)"><span class="check"></span><?php echo $cat->name ?></a></li>
+						<?php endforeach; ?>
 					</ul> <!-- /.cat-filter.new-media -->
 				</div> <!-- .span2 -->
 			</div> <!-- /.row -->
