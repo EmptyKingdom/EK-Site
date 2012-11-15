02d4dbb442099d5b624f50e474ae1633297fc9c6
jessica <jessica@exobi.com>
2012-11-15 13:32:26 -0500

    Hide featured image from page content

--- themes/ek2012/page.php
+++ themes/ek2012/page.php
@@ -1,4 +1,16 @@
-<?php get_header(); ?>
+<?php 
+function ek_hide_featured_image_from_content(){
+	global $post; 
+	{
+		$thumbnail_id = get_post_thumbnail_id($post->ID);	?>
+		<style type="text/css" media="screen">
+			.post-content .wp-image-<?php echo $thumbnail_id ?> {display: none}
+		</style>
+		<?php
+	}
+}
+add_action('wp_head', 'ek_hide_featured_image_from_content');
+get_header(); ?>
 <div class="row">
 	<div class="span8" id="main">
 	<?php if (have_posts()) : ?>
