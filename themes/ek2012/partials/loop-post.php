09720a56c66f4ad951088f113cb0dfa4b9f6b4d9
jessica <jessica@exobi.com>
2012-11-29 18:39:05 -0500

    Link to author pages

--- themes/ek2012/partials/loop-post.php
+++ themes/ek2012/partials/loop-post.php
@@ -1,6 +1,6 @@
 <div class="post <?php echo ek_get_cat($post, 'slug') ?> span4">
 	<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
-	<p class="author">by <?php the_author() ?></p>
+	<p class="author">by <?php the_author_posts_link() ?></p>
 	<p class="date"><?php the_time(get_option('date_format')) ?></p>
 	<div class="thumbnail"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a></div>
 	<h5 class="category"><?php echo ek_get_cat($post, 'name') ?></h5>
