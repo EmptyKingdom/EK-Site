ef6278da2a6f99320567b4d89206caf2d3ece1e0
jessica <jessica@exobi.com>
2013-01-17 01:37:53 -0500

    Fixed parent category not showing in post results when one category selected in filter

--- themes/ek2012/partials/loop-post.php
+++ themes/ek2012/partials/loop-post.php
@@ -1,9 +1,9 @@
-<div class="post <?php echo ek_get_cat($post, 'slug') ?> span4">
+<div class="post <?php echo ek_get_cat($post, 'slug', true) ?> span4">
 	<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
 	<p class="author">by <?php the_author_posts_link() ?></p>
 	<p class="date"><?php the_time(get_option('date_format')) ?></p>
 	<div class="thumbnail"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail() ?></a></div>
-	<h5 class="category"><?php echo ek_get_cat($post, 'name') ?></h5>
+	<h5 class="category"><?php echo ek_get_cat($post, 'name', true) ?></h5>
 	<div class="excerpt"><?php the_excerpt() ?></div>
 	<a class="view-more" href="<?php the_permalink() ?>">View More&hellip;</a>
 </div>
