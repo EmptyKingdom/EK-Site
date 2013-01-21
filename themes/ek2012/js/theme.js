c662bfa32e8dc76b2cd8ad954aaf5f0c09fd2cfa
jessica <jessica@exobi.com>
2013-01-21 16:13:09 -0500

    Social icons on post lists

--- themes/ek2012/js/theme.js
+++ themes/ek2012/js/theme.js
@@ -184,6 +184,21 @@ $(document).ready(function($){
 		}
 	}, 'a.video.vimeo');
 
+	$('.post .social a').click(function(event) {
+		event.preventDefault();
+		var width  = 575,
+		    height = 400,
+		    left   = ($(window).width()  - width)  / 2,
+		    top    = ($(window).height() - height) / 2,
+		    url    = this.href,
+		    opts   = 'status=1' +
+		             ',width='  + width  +
+		             ',height=' + height +
+		             ',top='    + top    +
+		             ',left='   + left
+		window.open(url, 'sharer', opts);
+	});
+
 	// cat filters
 	yepnope({
 		test: $('#cat-filters, #sfw-filter').length,
--- themes/ek2012/partials/loop-post.php
+++ themes/ek2012/partials/loop-post.php
@@ -5,6 +5,10 @@
 	elseif (preg_match('%vimeo\.com\/([0-9]*)%', $video_url, $code)) :
 		$provider = 'vimeo';
 	endif;
+	$sharer_title = urlencode($post->post_title);
+	$sharer_url = urlencode(get_permalink($post->ID));
+	$sharer_img = urlencode(wp_get_attachment_url(get_post_thumbnail_id()));
+	$sharer_descr = urlencode(get_the_excerpt());
  ?>
 <div class="post <?php echo ek_get_cat($post, 'slug', true) ?> span4">
 	<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
@@ -15,7 +19,15 @@
 		class="video <?php echo $provider ?>" data-provider="<?php echo $provider ?>" data-video_ref="<?php echo $code[1] ?>"><div></div
 		<?php endif; ?>
 		><?php the_post_thumbnail() ?></a></div>
-	<h5 class="category"><?php echo ek_get_cat($post, 'name', true) ?></h5>
+	<div class="clearfix">
+		<h5 class="category"><?php echo ek_get_cat($post, 'name', true) ?></h5>
+		<ul class="unstyled social">
+			<li class="facebook"><a href="http://www.facebook.com/share.php?s=100&p[url]=<?php echo $sharer_url ?>&p[title]=<?php echo $sharer_title ?>&p[summary]=<?php echo $sharer_descr ?>&p[images][0]=<?php echo $sharer_img ?>"></a></li>
+			<li class="twitter"><a href="https://twitter.com/share?url=<?php echo $sharer_url ?>"></a></li>
+			<li class="pinterest"><a href="https://pinterest.com/pin/create/button/?url=<?php echo $sharer_url ?>&media=<?php echo $sharer_img ?>&description=<?php echo $sharer_descr ?>"></a></li>
+			<!-- <li class="add"><a href=""></a></li> -->
+		</ul>
+	</div>
 	<div class="excerpt"><?php the_excerpt() ?></div>
 	<a class="view-more" href="<?php the_permalink() ?>">View More&hellip;</a>
 </div>
