f285e46710882c5fa1d5c16f224a0f94b1cc4ef2
jessica <jessica@exobi.com>
2013-01-24 13:57:59 -0500

    Archive titles

--- themes/ek2012/archive.php
+++ themes/ek2012/archive.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 <div class="row">
 	<div class="span8 <?php echo ek_get_cat(false, 'slug', true) ?>" id="main">
-		<?php if (is_category() && ! is_paged()) : ?>
+		<?php if (is_category() && ! is_paged() && ! get_queried_object()->parent) : ?>
 			<?php get_template_part('/partials/category', 'carousel') ?>
+		<?php elseif (is_category()) : ?>
+			<h1 class="sub"><?php echo get_queried_object()->name // subcat title ?></h1>
+		<?php endif; ?>
+		<?php if (is_tag()) : ?>
+			<h1>Posts tagged with "<?php echo get_queried_object()->name ?>"</h1>
 		<?php endif; ?>
 		<?php get_template_part('/partials/view-controls') ?>
 		<div id="post-list" class="post-list grid">
