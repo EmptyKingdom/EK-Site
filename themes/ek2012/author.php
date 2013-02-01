905dc9a2a359fe453eed4770a76c6b07a79b76ac
jessica <jessica@exobi.com>
2013-02-01 03:24:37 -0500

    cleaned up author fields, added acf fields for location / pinterest / facebook / twitter & implemented them

--- themes/ek2012/author.php
+++ themes/ek2012/author.php
@@ -1,6 +1,10 @@
 <?php
 global $wp_query;
 $curauth = $wp_query->get_queried_object();
+$pinterest = get_field('pinterest', 'user_'.$curauth->ID);
+$facebook = get_field('facebook', 'user_'.$curauth->ID);
+$twitter = get_field('twitter', 'user_'.$curauth->ID);
+$location = get_field('location', 'user_'.$curauth->ID);
 get_header(); ?>
 <div id="author-panel" class="container">
 	<div class="row">
@@ -13,26 +17,28 @@ get_header(); ?>
 					<h1><?php echo $curauth->display_name ?></h1>
 					<h2>Spy Name: <span><?php echo $curauth->first_name ?> <?php echo $curauth->last_name ?></span></h2>
 					<p><?php echo $curauth->description ?></p>
-					<div class="social pull-right">
+					<?php if ($pinterest || $facebook || $twitter) : ?>
+					<div class="social-links pull-right">
 						<p>Stalk me here: </p>
 						<ul class="unstyled ek-social" id="utility-menu">
-							<li class="pinterest icon">Pinterest</li>
-							<li class="facebook icon">Facebook</li>
-							<li class="twitter icon">Twitter</li>
+							<?php if ($pinterest) : ?><li class="pinterest icon"><a href="<?php echo $pinterest ?>" target="_blank">Pinterest</a></li><?php endif; ?>
+							<?php if ($facebook) : ?><li class="facebook icon"><a href="<?php echo $facebook ?>" target="_blank">Facebook</a></li><?php endif; ?>
+							<?php if ($twitter) : ?><li class="twitter icon"><a href="<?php echo $twitter ?>" target="_blank">Twitter</a></li><?php endif; ?>
 						</ul>
-					</div> <!-- /.social.pull-right -->
+					</div> <!-- /.social-links.pull-right -->
+					<?php endif; ?>
 				</div> <!-- /.span6.bio -->
 			</div> <!-- /.row -->
 		</div> <!-- /.span9.author-general -->
 		<div class="span3 author-details">
 			<h3>Location:</h3>
-			<p>Somewhere U.S.A.</p>
+			<p><?php echo $location ?></p>
 		</div> <!-- /.span3.author-details -->
 	</div> <!-- /.row -->
 </div> <!-- /#author-panel -->
 <h3 class="recent">Recent Posts By: <span><?php echo $curauth->display_name ?></span></h2>
 <div class="row">
-	<div class="span8 <?php echo ek_get_cat(false, 'slug', true) ?>" id="main">
+	<div class="span8" id="main">
 		<?php if (is_category()) : ?>
 			<?php get_template_part('/partials/category', 'carousel') ?>
 		<?php endif; ?>
@@ -40,14 +46,7 @@ get_header(); ?>
 		<div id="post-list" class="post-list grid">
 			<?php get_template_part('/partials/posts', 'listing') ?>
 		</div> <!-- /#post-list -->
-		<div class="row">
-			<div class="prev span4">
-				<h4><?php previous_posts_link('&laquo; Previous') ?></h4>
-			</div>
-			<div class="next span4">
-				<h4><?php next_posts_link('Next &raquo;') ?></h4>
-			</div>
-		</div>
+		<?php get_template_part('partials/posts-pagination') ?>
 	</div> <!-- /#main -->
 	<div class="span4" id="sidebar">
 		<?php get_sidebar(); ?>
