85c4f6fb23bc05595edd820e32fc5b463db45998
jessica <jessica@exobi.com>
2013-02-01 03:22:15 -0500

    removed featured image from page.php

--- themes/ek2012/page.php
+++ themes/ek2012/page.php
@@ -1,15 +1,4 @@
 <?php 
-function ek_hide_featured_image_from_content(){
-	global $post; 
-	{
-		$thumbnail_id = get_post_thumbnail_id($post->ID);	?>
-		<style type="text/css" media="screen">
-			.post-content .wp-image-<?php echo $thumbnail_id ?> {display: none}
-		</style>
-		<?php
-	}
-}
-add_action('wp_head', 'ek_hide_featured_image_from_content');
 get_header(); ?>
 <div class="row">
 	<div class="span8" id="main">
@@ -17,7 +6,6 @@ get_header(); ?>
 		<div class="post-full">
 		<?php while(have_posts()) : the_post(); ?>
 			<h1><?php the_title(); ?></h1>
-			<div class="featured-image"><?php the_post_thumbnail('full') ?></div>
 			<?php get_template_part('/partials/post', 'actions') ?>
 			<div class="post-content">
 				<?php the_content(); ?>
