7b85df6539ccc3ab6c5da21671789276f65f0b64
jessica <jessica@exobi.com>
2013-02-01 03:22:48 -0500

    put next / prev posts links into partial template

--- themes/ek2012/archive.php
+++ themes/ek2012/archive.php
@@ -13,14 +13,7 @@
 		<div id="post-list" class="post-list grid">
 			<?php get_template_part('/partials/posts', 'listing') ?>
 		</div> <!-- /#post-list -->
-		<div class="row" id="posts-pagination">
-			<div class="offset4 span4">
-				<div class="row">
-					<h4 class="span2 prev"><?php previous_posts_link('Previous') ?></h4>
-					<h4 class="span2 next"><?php next_posts_link('Next') ?></h4>
-				</div>
-			</div>
-		</div>
+		<?php get_template_part('partials/posts-pagination') ?>
 	</div> <!-- /#main -->
 	<div class="span4" id="sidebar">
 		<?php get_sidebar(); ?>
---
+++ themes/ek2012/partials/posts-pagination.php
@@ -0,0 +1,8 @@
+		<div class="row" id="posts-pagination">
+			<div class="offset4 span4">
+				<div class="row">
+					<h4 class="span2 prev"><?php previous_posts_link('Previous') ?></h4>
+					<h4 class="span2 next"><?php next_posts_link('Next') ?></h4>
+				</div>
+			</div>
+		</div>
