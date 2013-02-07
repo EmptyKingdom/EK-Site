e1bb0dde89f8b239260b8eeb3d2b8d3f71b645c5
jessica <jessica@exobi.com>
2013-02-06 20:50:48 -0500

    Simplified mobile layout

--- themes/ek2012/footer.php
+++ themes/ek2012/footer.php
@@ -7,29 +7,27 @@
 					</div> <!-- /.span4 -->
 					<div class="span4" id="footer-middle">
 						<div class="row">
-							<div class="span2">
+							<div class="span2 quicklinks">
 								<h4>EK Quicklinks</h4>
-							</div>
-							<div class="span1">
+								<div class="row">
+									<div class="span1 footer-nav">
+									<?php wp_nav_menu(array(
+										'theme_location' => 'quick-links-footer',
+										'container_class' => '',
+										'menu_class' => '',
+										'items_wrap'      => '<ul id="%1$s" class="unstyled">%3$s</ul>',
+										'fallback_cb' => false,
+									)) ?>
+									</div> <!-- /.span1 -->
+									<div class="span1 footer-categories">
+										<ul class="unstyled">
+											<?php wp_list_categories('depth=1&title_li=') ?>
+										</ul>
+									</div> <!-- /.span1 -->
+								</div> <!-- /.row -->
+							</div> <!-- /.span2 -->
+							<div class="span2 authors">
 								<h4>Authors</h4>
-							</div>
-						</div>
-						<div class="row">
-							<div class="span1">
-							<?php wp_nav_menu(array(
-								'theme_location' => 'quick-links-footer',
-								'container_class' => '',
-								'menu_class' => '',
-								'items_wrap'      => '<ul id="%1$s" class="unstyled">%3$s</ul>',
-								'fallback_cb' => false,
-							)) ?>
-							</div> <!-- /.span1 -->
-							<div class="span1">
-								<ul class="unstyled">
-									<?php wp_list_categories('depth=1&title_li=') ?>
-								</ul>
-							</div> <!-- /.span1 -->
-							<div class="span2">
 								<ul class="unstyled">
 									<?php wp_list_authors('depth=1&title_li=') ?>
 								</ul>
