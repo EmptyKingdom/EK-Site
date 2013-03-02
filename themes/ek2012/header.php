edf7647321da3978b5aa0fc131f68902a10a9030
jessica <jessica@exobi.com>
2013-03-02 02:40:34 -0500

    remove rogue letter s

--- themes/ek2012/header.php
+++ themes/ek2012/header.php
@@ -55,7 +55,7 @@
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
-		//]]></script>s
+		//]]></script>
 	</head>
 	<body <?php body_class() ?>>
 		<div id="fb-root"></div>
