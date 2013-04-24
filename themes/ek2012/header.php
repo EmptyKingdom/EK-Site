d845770f49ae1c1c4018d75b7bc4f1f20d9d56aa
ricobot <ricardo.ortiz@gmail.com>
2013-04-24 09:43:44 -0400

    Added DFP Publisher Tags
    Added a 300x250 Ad unit to EK Sidebar

--- themes/ek2012/header.php
+++ themes/ek2012/header.php
@@ -46,6 +46,29 @@
 		});
 		</script>
 
+		<script type='text/javascript'>
+		var googletag = googletag || {};
+		googletag.cmd = googletag.cmd || [];
+		(function() {
+			var gads = document.createElement('script');
+			gads.async = true;
+			gads.type = 'text/javascript';
+			var useSSL = 'https:' == document.location.protocol;
+			gads.src = (useSSL ? 'https:' : 'http:') + 
+			'//www.googletagservices.com/tag/js/gpt.js';
+			var node = document.getElementsByTagName('script')[0];
+			node.parentNode.insertBefore(gads, node);
+		})();
+		</script>
+
+		<script type='text/javascript'>
+		googletag.cmd.push(function() {
+		googletag.defineSlot('/59182986/JonathanWongMV-300x250-EK-Q2', [300, 250], 'div-gpt-ad-1366810643930-0').addService(googletag.pubads());
+		googletag.pubads().enableSingleRequest();
+		googletag.enableServices();
+		});
+		</script>
+
 		<?php wp_head(); ?>
 		<script type="text/javascript">//<![CDATA[
 		var _gaq = _gaq || [];
