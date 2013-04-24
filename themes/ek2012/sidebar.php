e02ac130854b57e067719a5862d10868876b311c
jessica <jessica@exobi.com>
2013-04-24 13:05:02 -0400

    Take top ad out of sidebar

--- themes/ek2012/sidebar.php
+++ themes/ek2012/sidebar.php
@@ -1,12 +1,2 @@
-		<!-- Ad slot -->
-		<div id="ad-slot-1" class="ad-slot">
-			<!-- EK_ATF_Sidebar_300x250 -->
-			<div id='div-gpt-ad-1358972561550-0'>
-			<script type='text/javascript'>
-			googletag.cmd.push(function() { googletag.display('div-gpt-ad-1358972561550-0'); });
-			</script>
-			</div>
-		</div> <!-- /#ad-slot-1 -->
-
 		<?php dynamic_sidebar('Right Sidebar') ?>
 		
